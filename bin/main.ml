49413749954290df336f13927423b428c7bcaf5d
azimut <azimut.github@protonmail.com>
2024-03-16 04:04:00 -0300

    elapsed time clock, timestamp from int->float

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,8 @@
-type command = Eof | Nop | Timestamp of int
+type command = Eof | Nop | Timestamp of float
 
 let parse_timestamp t =
   Scanf.sscanf t "%d:%d:%d.%d" (fun hour minute second _ ->
-      (hour * 60 * 60) + (minute * 60) + second )
+      Float.of_int ((hour * 60 * 60) + (minute * 60) + second) )
 
 let rec read_duration () =
   match In_channel.input_line stdin with
@@ -23,32 +23,36 @@ let line2command line =
         (parse_timestamp @@ String.(sub line start (length line - start)))
   | _ -> Nop
 
-let bar perc =
+let bar perc start_time =
   let duplicate n s = String.concat "" @@ List.init n (fun _ -> s) in
-  Printf.sprintf "%3d|%s%s|??:??:??" perc
+  let tt = Unix.gmtime (Unix.time () -. start_time) in
+  Printf.sprintf "%3d|%s%s|%02d:%02d:%02d" perc
     (duplicate perc "\u{2588}")
     (duplicate (100 - perc) " ")
+    tt.tm_hour tt.tm_min tt.tm_sec
 
-let progress_bar duration timestamp =
-  let percentage a b = Int.(of_float (100.0 *. (to_float b /. to_float a))) in
+let progress_bar duration timestamp start_time =
+  let percentage = Int.of_float (100.0 *. timestamp /. duration) in
   Tty.Escape_seq.cursor_horizontal_seq 0 () ;
   Tty.Escape_seq.erase_line_seq 200 () ;
-  print_string @@ bar @@ percentage duration timestamp
+  print_string @@ bar percentage start_time
 
 let read_command () =
   match In_channel.input_line stdin with
   | None -> Eof
   | Some line -> line2command line
 
-let rec read_commands duration =
+let rec read_commands duration start_time =
   match read_command () with
   | Eof -> ()
-  | Nop -> read_commands duration
+  | Nop -> read_commands duration start_time
   | Timestamp timestamp ->
-      progress_bar duration timestamp ;
-      read_commands duration
+      progress_bar duration timestamp start_time ;
+      read_commands duration start_time
 
 let () =
   match read_duration () with
   | Error err -> failwith err
-  | Ok duration -> read_commands duration
+  | Ok duration ->
+      let start_time = Unix.time () in
+      read_commands duration start_time
