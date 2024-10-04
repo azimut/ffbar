20c7c26693914a9fbf373be02071d99047b5f66c
azimut <azimut.github@protonmail.com>
2024-10-04 09:51:37 -0300

    add some tests

--- test/test_ffbar.ml
+++ test/test_ffbar.ml
@@ -1,3 +1,15 @@
-let () =
+let dur_timestamp () =
+  let ic = Stdlib.open_in "Test_parsing.input" in
+  try Ffbar.read_output ic (Some "00:00:01") None
+  with e -> close_in_noerr ic ; raise e
+
+let durseek_timestamp () =
+  let ic = Stdlib.open_in "Test_parsing.input" in
+  try Ffbar.read_output ic (Some "00:00:01") (Some "00:00:01")
+  with e -> close_in_noerr ic ; raise e
+
+let no_timestamp () =
   let ic = Stdlib.open_in "Test_parsing.input" in
   try Ffbar.read_output ic None None with e -> close_in_noerr ic ; raise e
+
+let () = no_timestamp () ; dur_timestamp () ; durseek_timestamp ()
